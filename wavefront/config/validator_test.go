e2f3ae99779b4866cd57b0442f31f8cd2526a944
Venil Noronha <veniln@vmware.com>
2018-09-01 10:13:05 -0700

    Format validator test code
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- wavefront/config/validator_test.go
+++ wavefront/config/validator_test.go
@@ -27,15 +27,39 @@ func TestValidateCredentials(t *testing.T) {
 		err    error
 	}{
 		{config.Params{Credentials: nil}, config.NoCredentialsError},
-		{config.Params{Credentials: &config.Params_Direct{Direct: nil}}, config.NoCredentialsError},
-		{config.Params{Credentials: &config.Params_Proxy{Proxy: nil}}, config.NoCredentialsError},
-		{config.Params{Credentials: &config.Params_Direct{Direct: &config.Params_WavefrontDirect{Server: ""}}}, config.InvalidDirectCredsError},
-		{config.Params{Credentials: &config.Params_Direct{Direct: &config.Params_WavefrontDirect{Server: "not-a-valid-uri", Token: "dummy-token"}}}, fmt.Errorf("parse not-a-valid-uri: invalid URI for request")},
-		{config.Params{Credentials: &config.Params_Direct{Direct: &config.Params_WavefrontDirect{Token: ""}}}, config.InvalidDirectCredsError},
-		{config.Params{Credentials: &config.Params_Direct{Direct: &config.Params_WavefrontDirect{Server: "https://server.wavefront.com", Token: "dummy-token"}}}, nil},
-		{config.Params{Credentials: &config.Params_Proxy{Proxy: &config.Params_WavefrontProxy{Address: ""}}}, config.InvalidProxyCredsError},
-		{config.Params{Credentials: &config.Params_Proxy{Proxy: &config.Params_WavefrontProxy{Address: "not-a-valid-address"}}}, fmt.Errorf("address not-a-valid-address: missing port in address")},
-		{config.Params{Credentials: &config.Params_Proxy{Proxy: &config.Params_WavefrontProxy{Address: "192.168.99.100:8080"}}}, nil},
+		{config.Params{Credentials: &config.Params_Direct{
+			Direct: nil,
+		}}, config.NoCredentialsError},
+		{config.Params{Credentials: &config.Params_Proxy{
+			Proxy: nil,
+		}}, config.NoCredentialsError},
+		{config.Params{Credentials: &config.Params_Direct{
+			Direct: &config.Params_WavefrontDirect{Server: ""},
+		}}, config.InvalidDirectCredsError},
+		{config.Params{Credentials: &config.Params_Direct{
+			Direct: &config.Params_WavefrontDirect{
+				Server: "not-a-valid-uri",
+				Token:  "dummy-token",
+			},
+		}}, fmt.Errorf("parse not-a-valid-uri: invalid URI for request")},
+		{config.Params{Credentials: &config.Params_Direct{
+			Direct: &config.Params_WavefrontDirect{Token: ""},
+		}}, config.InvalidDirectCredsError},
+		{config.Params{Credentials: &config.Params_Direct{
+			Direct: &config.Params_WavefrontDirect{
+				Server: "https://server.wavefront.com",
+				Token:  "dummy-token",
+			},
+		}}, nil},
+		{config.Params{Credentials: &config.Params_Proxy{
+			Proxy: &config.Params_WavefrontProxy{Address: ""},
+		}}, config.InvalidProxyCredsError},
+		{config.Params{Credentials: &config.Params_Proxy{
+			Proxy: &config.Params_WavefrontProxy{Address: "not-a-valid-address"},
+		}}, fmt.Errorf("address not-a-valid-address: missing port in address")},
+		{config.Params{Credentials: &config.Params_Proxy{
+			Proxy: &config.Params_WavefrontProxy{Address: "192.168.99.100:8080"},
+		}}, nil},
 	}
 
 	for _, entry := range table {
@@ -55,10 +79,38 @@ func TestValidateMetrics(t *testing.T) {
 		{config.Params_MetricInfo{Type: config.GAUGE}, nil},
 		{config.Params_MetricInfo{Type: config.COUNTER}, nil},
 		{config.Params_MetricInfo{Type: config.DELTA_COUNTER}, nil},
-		{config.Params_MetricInfo{Name: "metric-name", Type: config.HISTOGRAM}, err},
-		{config.Params_MetricInfo{Name: "metric-name", Type: config.HISTOGRAM, Sample: &config.Params_MetricInfo_Sample{}}, err},
-		{config.Params_MetricInfo{Name: "metric-name", Type: config.HISTOGRAM, Sample: &config.Params_MetricInfo_Sample{Definition: &config.Params_MetricInfo_Sample_ExpDecay_{ExpDecay: &config.Params_MetricInfo_Sample_ExpDecay{ReservoirSize: 1024, Alpha: 0.015}}}}, nil},
-		{config.Params_MetricInfo{Name: "metric-name", Type: config.HISTOGRAM, Sample: &config.Params_MetricInfo_Sample{Definition: &config.Params_MetricInfo_Sample_Uniform_{Uniform: &config.Params_MetricInfo_Sample_Uniform{ReservoirSize: 1024}}}}, nil},
+		{config.Params_MetricInfo{
+			Name: "metric-name",
+			Type: config.HISTOGRAM,
+		}, err},
+		{config.Params_MetricInfo{
+			Name:   "metric-name",
+			Type:   config.HISTOGRAM,
+			Sample: &config.Params_MetricInfo_Sample{},
+		}, err},
+		{config.Params_MetricInfo{
+			Name: "metric-name",
+			Type: config.HISTOGRAM,
+			Sample: &config.Params_MetricInfo_Sample{
+				Definition: &config.Params_MetricInfo_Sample_ExpDecay_{
+					ExpDecay: &config.Params_MetricInfo_Sample_ExpDecay{
+						ReservoirSize: 1024,
+						Alpha:         0.015,
+					},
+				},
+			},
+		}, nil},
+		{config.Params_MetricInfo{
+			Name: "metric-name",
+			Type: config.HISTOGRAM,
+			Sample: &config.Params_MetricInfo_Sample{
+				Definition: &config.Params_MetricInfo_Sample_Uniform_{
+					Uniform: &config.Params_MetricInfo_Sample_Uniform{
+						ReservoirSize: 1024,
+					},
+				},
+			},
+		}, nil},
 	}
 
 	for _, entry := range table {
